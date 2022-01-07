43dbfbb0a757ebc51462eadeba217196dab3aa8a
URBACZKA IVAN <46192105+IVAN-URBACZKA@users.noreply.github.com>
2022-01-07 09:58:36 +0100

    Base du site

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import ProjectPost
+from .models import ProjectPost, CategoryPost
 
 
 class ProjectPostAdmin(admin.ModelAdmin):
-    fields = ('title', 'image', 'content', 'slug')
+    fields = ('title', 'image', 'content', 'slug', 'category')
 
 
 admin.site.register(ProjectPost, ProjectPostAdmin)
+
+class CategoryPostAdmin(admin.ModelAdmin):
+    fields = ('title',)
+
+admin.site.register(CategoryPost, CategoryPostAdmin)
--- src/projects/models.py
+++ src/projects/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
+class CategoryPost(models.Model):
+
+    title = models.CharField(max_length=30, verbose_name="title cat")
+
+    class Meta:
+        verbose_name = "Catégorie"
+
+    def __str__(self):
+        return self.title
 
 class ProjectPost(models.Model):
 
     title = models.CharField(max_length=30, verbose_name="Titre")
+    category = models.ManyToManyField(CategoryPost)
     content = models.TextField()
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=250)
     image = models.ImageField(upload_to='portfolio')
 
 
